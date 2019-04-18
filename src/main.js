3c6947de2caf3d0d6085943e51134078ebb20492
chenghaolun <1194234765@qq.com>
2019-04-19 07:17:11 +0800

    delete Array.from()

--- src/main.js
+++ src/main.js
@@ -11,11 +11,11 @@ const createVApp = (count) => createElement('div', {
 	children: [
 		createElement('input'), // dom重绘使得Input失焦
 		String(count),
-		...Array.from({ length: count }, () => createElement('img', {
+		createElement('img', {
 			attrs: {
 				src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555610261877&di=6619e67b4f45768a359a296c55ec1cc3&imgtype=0&src=http%3A%2F%2Fimg.bimg.126.net%2Fphoto%2Fmr7DezX-Q4GLNBM_VPVaWA%3D%3D%2F333829322379622331.jpg'
 			}
-		}))
+		})
 	]
 })
 
